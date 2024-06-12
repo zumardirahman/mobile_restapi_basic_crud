4cd77ab6571c0ea520e70e805e9ac2e465257e7a
zumardirahman <zumar2014@gmail.com>
2024-06-13 02:42:31 +0700

    fix (cors): penamabahan header

--- koneksi.php
+++ koneksi.php
@@ -1,7 +1,12 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Content-type:application/json;charset=utf-8"); 
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
 $host = "localhost"; // Ganti dengan host Anda
 $username = "root"; // Ganti dengan username database Anda
-$password = ""; // Ganti dengan password database Anda
+$password = "toor"; // Ganti dengan password database Anda
 $database = "data_toko"; // Ganti dengan nama database Anda
 
 // Membuat koneksi
